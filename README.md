078a9f4dc660ca7051a5ea21c8386c660c23356e
Skylar Kim <kimsooye@usc.edu>
2020-05-21 14:07:08 -0700

    Added background generator
    Added the index.html, README.md, script.js, and style.css

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# backgroundgenerator
+# Background Generator 
  Zero to Mastery Udemy Course Lecture 135
+
+### Input Type: Color
+`<input>` elements of type __color__ provide a UI element that lets a user specify a color.
+Example in Code:
+```html
+<input class="color1" type="color" name="color1" value="#ff0000">
+<input class="color2" type="color" name="color2" value="#00ff00">
+```
+
+We want to add an event listener to the color input, but the color input isn't necessarily a click, mouseenter, or a keypress. Instead it's just called __input__.
+```javascript
+// add an event listener to the two color inputs
+color1.addEventListener("input", setGradient)
+color2.addEventListener("input", setGradient)
+```
+### CSS Linear Gradient
+[CSS linear-gradient() Function W3 Schools](https://www.w3schools.com/cssref/func_linear-gradient.asp)
+The syntax used in the CSS file:
+```css
+background-image: linear-gradient(direction, color-stop1, color-stop2, ...);
+```
---
+++ script.js
@@ -0,0 +1,44 @@
+var css = document.querySelector("h3");
+// color1 and color2 is a class, so that's why the syntax is
+// .color1 .color2
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var random = document.querySelector("button");
+
+
+function setGradient() {
+	// The background element is in <body> tag
+	// So we much change the linear-gradient values in the body element
+	// in the style.css file
+	body.style.background = "linear-gradient(to right," 
+	+ color1.value + ", " + color2.value  +")";
+
+	// console.log("Set Gradient: " + color1.value);
+	// console.log("Set Gradient: " + color2.value);
+
+	css.textContent = body.style.background + ";"
+}
+
+function setGradientRandom() {
+	// Random hex generator is from:
+	// https://css-tricks.com/snippets/javascript/random-hex-color/
+	// assign two variables to two random hex colors
+	var randcolor1 = "#" + Math.floor(Math.random()*16777215).toString(16);
+	var randcolor2 = "#" + Math.floor(Math.random()*16777215).toString(16);
+
+	body.style.background = "linear-gradient(to right," 
+	+ randcolor1 + ", " + randcolor2  +")";
+
+	// console.log("Set RandGradient: " + randcolor1 );
+	// console.log("Set RandGradient: " + randcolor2);	
+
+	css.textContent = body.style.background + ";"
+}
+
+// add an event listener to the two color inputs
+color1.addEventListener("input", setGradient)
+color2.addEventListener("input", setGradient)
+
+//
+random.addEventListener("click", setGradientRandom)
