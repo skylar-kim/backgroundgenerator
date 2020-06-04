562b5399a5a7eaeac3baee9019edd673241dde71
Skylar Kim <kimsooye@usc.edu>
2020-06-04 16:20:55 -0400

    update backgroundgenerator with npm stuff

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(array, 3));
 var css = document.querySelector("h3");
 // color1 and color2 is a class, so that's why the syntax is
 // .color1 .color2
